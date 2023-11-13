6b0c2ab980cd594a7b304d6c8d5ef1f0d00abd67
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-11-14 00:59:34 +0900

    feat: 클래스가 빈 패키지인 경우 빈 list를 반환하도록 수정

--- src/main/java/hibernate/binder/ComponentScanner.java
+++ src/main/java/hibernate/binder/ComponentScanner.java
@@ -1,6 +1,7 @@
 package hibernate.binder;
 
 import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,10 +10,16 @@ public class ComponentScanner {
     private ComponentScanner() {
     }
 
-    public static List<Class<?>> scan(String basePackage) throws ClassNotFoundException {
+    public static List<Class<?>> scan(String basePackage) {
         List<Class<?>> classes = new ArrayList<>();
         String path = basePackage.replace(".", "/");
-        File baseDir = new File(Thread.currentThread().getContextClassLoader().getResource(path).getFile());
+
+        URL resource = Thread.currentThread().getContextClassLoader().getResource(path);
+        if (resource == null) {
+            return List.of();
+        }
+
+        File baseDir = new File(resource.getFile());
 
         if (baseDir.exists() && baseDir.isDirectory()) {
             for (File file : baseDir.listFiles()) {
@@ -20,10 +27,18 @@ public class ComponentScanner {
                     classes.addAll(scan(basePackage + "." + file.getName()));
                 } else if (file.getName().endsWith(".class")) {
                     String className = basePackage + "." + file.getName().substring(0, file.getName().length() - 6);
-                    classes.add(Class.forName(className));
+                    classes.add(parseClassByName(className));
                 }
             }
         }
         return classes;
+    }
+
+    private static Class<?> parseClassByName(String className)  {
+        try {
+            return Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("클래스를 찾을 수 없습니다.");
+        }
     }
 }
