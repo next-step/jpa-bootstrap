1dda52d8384f5d09bb3ca73767821e2ba6ada0f6
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-11-14 20:48:10 +0900

    feat) Employee, Department 엔티티 클래스 추가

---
+++ src/main/java/domain/Department.java
@@ -0,0 +1,20 @@
+package domain;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name = "department_id")
+    private List<Employee> employees;
+
+}
