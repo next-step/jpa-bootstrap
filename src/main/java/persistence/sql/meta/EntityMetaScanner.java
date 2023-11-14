c2c31e35974d47667a824d98a0e455e6219e38a6
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-11-14 20:35:04 +0900

    feat) EntityMetaScanner 클래스 추가

---
+++ src/main/java/persistence/sql/meta/EntityMetaScanner.java
@@ -0,0 +1,36 @@
+package persistence.sql.meta;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntityMetaScanner {
+
+    public List<Class<?>> scan(String basePackage) throws IOException, ClassNotFoundException {
+        List<Class<?>> classes = new ArrayList<>();
+        String path = basePackage.replace(".", "/");
+        File baseDir = new File(Thread.currentThread().getContextClassLoader().getResource(path).getFile());
+
+        if (!baseDir.exists() || !baseDir.isDirectory()) {
+            return classes;
+        }
+        for (File file : baseDir.listFiles()) {
+            addClasses(basePackage, file, classes);
+        }
+        return classes;
+    }
+
+    private void addClasses(String basePackage, File file, List<Class<?>> classes) throws IOException, ClassNotFoundException {
+        if (file.isDirectory()) {
+            classes.addAll(scan(basePackage + "." + file.getName()));
+            return;
+        }
+        if (file.getName().endsWith(".class")) {
+            String className = basePackage + "." + file.getName().substring(0, file.getName().length() - 6);
+            classes.add(Class.forName(className));
+        }
+    }
+
+
+}
