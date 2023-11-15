7e8041690ce440684db94cd7bbe6edc42d16fed7
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-11-15 20:33:08 +0900

    refactor) BASE_PACKAGE 상수 위치 이동

--- src/main/java/persistence/sql/meta/EntityMetaScanner.java
+++ src/main/java/persistence/sql/meta/EntityMetaScanner.java
@@ -9,34 +9,39 @@ import java.util.List;
 
 public class EntityMetaScanner {
 
-    public List<Class<?>> scan(String basePackage) throws IOException, ClassNotFoundException {
+    private static final String BASE_PACKAGE = "domain";
+
+    public List<Class<?>> scan() throws IOException, ClassNotFoundException {
+        return scan(BASE_PACKAGE);
+    }
+
+    private List<Class<?>> scan(String packageDir) throws IOException, ClassNotFoundException {
         List<Class<?>> classes = new ArrayList<>();
-        String path = basePackage.replace(".", "/");
+        String path = packageDir.replace(".", "/");
         File baseDir = new File(Thread.currentThread().getContextClassLoader().getResource(path).getFile());
 
         if (!baseDir.exists() || !baseDir.isDirectory()) {
             return classes;
         }
         for (File file : baseDir.listFiles()) {
-            addClasses(basePackage, file, classes);
+            addClasses(packageDir, file, classes);
         }
         return classes;
     }
 
-    private void addClasses(String basePackage, File file, List<Class<?>> classes) throws IOException, ClassNotFoundException {
+    private void addClasses(String packageDir, File file, List<Class<?>> classes) throws IOException, ClassNotFoundException {
         if (file.isDirectory()) {
-            classes.addAll(scan(basePackage + "." + file.getName()));
+            classes.addAll(scan(packageDir + "." + file.getName()));
             return;
         }
         if (!file.getName().endsWith(".class")) {
             return;
         }
-        String className = basePackage + "." + file.getName().substring(0, file.getName().length() - 6);
+        String className = packageDir + "." + file.getName().substring(0, file.getName().length() - 6);
         Class<?> addTarget = Class.forName(className);
         if (addTarget.isAnnotationPresent(Entity.class)) {
             classes.add(addTarget);
         }
     }
-
 
 }
