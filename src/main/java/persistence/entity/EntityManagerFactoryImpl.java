526ccdeff05f0d4171671e3fb84030e674cce2fc
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-11-16 21:28:19 +0900

    feat) EntityManagerFactoryImpl 클래스 구현

---
+++ src/main/java/persistence/entity/EntityManagerFactoryImpl.java
@@ -0,0 +1,34 @@
+package persistence.entity;
+
+import jdbc.JdbcTemplate;
+
+import java.sql.Connection;
+
+public class EntityManagerFactoryImpl implements EntityManagerFactory {
+
+    private final CurrentSessionContext currentSessionContext;
+
+    private final Connection connection;
+
+    private EntityManagerFactoryImpl(CurrentSessionContext currentSessionContext, Connection connection) {
+        this.currentSessionContext = currentSessionContext;
+        this.connection = connection;
+    }
+
+    public static EntityManagerFactoryImpl of(Connection connection) {
+        DefaultCurrentSessionContext sessionContext = DefaultCurrentSessionContext.getInstance();
+        return new EntityManagerFactoryImpl(sessionContext, connection);
+    }
+
+    @Override
+    public EntityManager openSession() {
+        EntityManager entityManager = currentSessionContext.currentSession();
+        if (entityManager != null) {
+            throw new IllegalStateException("세션 생성이 완료되었습니다.");
+        }
+        DefaultEntityManager defaultEntityManager = DefaultEntityManager.of(new JdbcTemplate(connection));
+        currentSessionContext.openSession(Thread.currentThread(), defaultEntityManager);
+        return defaultEntityManager;
+    }
+
+}
