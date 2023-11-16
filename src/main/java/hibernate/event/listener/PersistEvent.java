a3f84a149d6ea82d196fc1004dd751e5387b97e8
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-11-17 01:08:13 +0900

    feat: PersistEvent 생성 및 테스트

---
+++ src/main/java/hibernate/event/listener/PersistEvent.java
@@ -0,0 +1,27 @@
+package hibernate.event.listener;
+
+import hibernate.entity.EntityPersister;
+import hibernate.metamodel.MetaModel;
+
+public class PersistEvent {
+
+    private final EntityPersister<?> entityPersister;
+    private final Object entity;
+
+    private PersistEvent(EntityPersister<?> entityPersister, Object entity) {
+        this.entityPersister = entityPersister;
+        this.entity = entity;
+    }
+
+    public static PersistEvent createEvent(final MetaModel metaModel, final Object entity) {
+        return new PersistEvent(metaModel.getEntityPersister(entity.getClass()), entity);
+    }
+
+    public EntityPersister<?> getEntityPersister() {
+        return entityPersister;
+    }
+
+    public Object getEntity() {
+        return entity;
+    }
+}
---
+++ src/test/java/hibernate/event/listener/PersistEventTest.java
@@ -0,0 +1,35 @@
+package hibernate.event.listener;
+
+import hibernate.metamodel.BasicMetaModel;
+import hibernate.metamodel.MetaModel;
+import hibernate.metamodel.MetaModelImpl;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class PersistEventTest {
+
+    private final MetaModel metamodel = MetaModelImpl.createPackageMetaModel(
+            BasicMetaModel.createPackageMetaModel("hibernate.event.listener"),
+            null
+    );
+
+    @Test
+    void PersistEvent를_생성한다() {
+        TestEntity givenEntity = new TestEntity();
+        PersistEvent actual = PersistEvent.createEvent(metamodel, givenEntity);
+        assertAll(
+                () -> assertThat(actual.getEntity()).isEqualTo(givenEntity),
+                () -> assertThat(actual.getEntityPersister()).isNotNull()
+        );
+    }
+
+    @Entity
+    private static class TestEntity {
+        @Id
+        private Long id;
+    }
+}
