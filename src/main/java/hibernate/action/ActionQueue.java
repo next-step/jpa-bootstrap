74af14acdbacd17bf74003e1ec7952369c55092f
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-11-18 00:47:14 +0900

    feat: ActionQueue action 추가 기능

--- src/main/java/hibernate/action/ActionQueue.java
+++ src/main/java/hibernate/action/ActionQueue.java
@@ -1,4 +1,32 @@
 package hibernate.action;
 
+import java.util.Queue;
+
 public class ActionQueue {
+
+    private final Queue<EntityInsertAction<?>> insertions;
+    private final Queue<EntityUpdateAction<?>> updates;
+    private final Queue<EntityDeleteAction<?>> deletions;
+
+    public ActionQueue(
+            final Queue<EntityInsertAction<?>> insertions,
+            final Queue<EntityUpdateAction<?>> updates,
+            final Queue<EntityDeleteAction<?>> deletions
+    ) {
+        this.insertions = insertions;
+        this.updates = updates;
+        this.deletions = deletions;
+    }
+
+    public void addAction(final EntityInsertAction<?> action) {
+        insertions.add(action);
+    }
+
+    public void addAction(final EntityUpdateAction<?> action) {
+        updates.add(action);
+    }
+
+    public void addAction(final EntityDeleteAction<?> action) {
+        deletions.add(action);
+    }
 }
