631ac05544c7473bc7298c1336db39280d5a0c1f
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-11-19 14:45:35 +0900

    refactor) DefaultCurrentSessionContext 정적구조변경 적용

--- src/main/java/persistence/entity/EntityManagerFactoryImpl.java
+++ src/main/java/persistence/entity/EntityManagerFactoryImpl.java
@@ -6,28 +6,24 @@ import java.sql.Connection;
 
 public class EntityManagerFactoryImpl implements EntityManagerFactory {
 
-    private final CurrentSessionContext currentSessionContext;
-
     private final Connection connection;
 
-    private EntityManagerFactoryImpl(CurrentSessionContext currentSessionContext, Connection connection) {
-        this.currentSessionContext = currentSessionContext;
+    private EntityManagerFactoryImpl(Connection connection) {
         this.connection = connection;
     }
 
     public static EntityManagerFactory of(Connection connection) {
-        DefaultCurrentSessionContext sessionContext = DefaultCurrentSessionContext.getInstance();
-        return new EntityManagerFactoryImpl(sessionContext, connection);
+        return new EntityManagerFactoryImpl(connection);
     }
 
     @Override
     public EntityManager openSession() {
-        EntityManager entityManager = currentSessionContext.currentSession();
+        EntityManager entityManager = DefaultCurrentSessionContext.currentSession();
         if (entityManager != null) {
             throw new IllegalStateException("세션 생성이 완료되었습니다.");
         }
         DefaultEntityManager defaultEntityManager = DefaultEntityManager.of(new JdbcTemplate(connection));
-        currentSessionContext.openSession(Thread.currentThread(), defaultEntityManager);
+        DefaultCurrentSessionContext.openSession(defaultEntityManager);
         return defaultEntityManager;
     }
 
