efcc637308eb7e6e4d6d539b9b09f7d11daa3ad1
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-11-19 14:46:18 +0900

    remove) CurrentSessionContext 인터페이스 삭제

--- src/main/java/persistence/entity/CurrentSessionContext.java
+++
@@ -1,8 +0,0 @@
-package persistence.entity;
-
-public interface CurrentSessionContext {
-
-    void openSession(Thread thread, EntityManager entityManager);
-
-    EntityManager currentSession();
-}
