69e10ec8c94a07b794436e14eafa91d3344bd075
Jongmin Yoon <jongmin4943@gmail.com>
2023-11-23 22:18:06 +0900

    fix : EventType 코드 정리

--- src/main/java/persistence/event/EventType.java
+++ src/main/java/persistence/event/EventType.java
@@ -1,16 +1,8 @@
 package persistence.event;
 
 public enum EventType {
-
-    PERSIST(PersistEventListener.class),
-    MERGE(MergeEventListener.class),
-    DELETE(DeleteEventListener.class),
-    LOAD(LoadEventListener.class);
-
-    private final Class<? extends EventListener> listenerClass;
-
-    EventType(final Class<? extends EventListener> listenerClass) {
-        this.listenerClass = listenerClass;
-    }
-
+    PERSIST,
+    MERGE,
+    DELETE,
+    LOAD
 }
