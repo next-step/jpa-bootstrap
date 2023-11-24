c157264ba2e70de42e278afc511fabbb4f990f5b
mj950425 <minjoon.lee@myrealtrip.com>
2023-11-24 20:22:08 +0900

    ActionQueue 중복 제거, 사용한 액션은 제거

--- src/main/java/persistence/actionqueue/ActionQueue.java
+++ src/main/java/persistence/actionqueue/ActionQueue.java
@@ -7,11 +7,13 @@ public class ActionQueue {
     private final Queue<EntityAction> actions = new LinkedList<>();
 
     public void addAction(EntityAction action) {
+
         actions.add(action);
     }
 
     public void executeActions() {
-        for (EntityAction action : actions) {
+        while (!actions.isEmpty()) {
+            EntityAction action = actions.poll();
             action.execute();
         }
     }
--- src/main/java/persistence/actionqueue/EntityAction.java
+++ src/main/java/persistence/actionqueue/EntityAction.java
@@ -2,6 +2,8 @@ package persistence.actionqueue;
 
 import persistence.entity.persister.EntityPersister;
 
+import java.util.Objects;
+
 public abstract class EntityAction {
     protected final Object instance;
     protected final Object snapshot;
@@ -14,4 +16,17 @@ public abstract class EntityAction {
     }
 
     public abstract void execute();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EntityAction)) return false;
+        EntityAction that = (EntityAction) o;
+        return Objects.equals(instance, that.instance) && Objects.equals(snapshot, that.snapshot) && Objects.equals(persister, that.persister);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(instance, snapshot, persister);
+    }
 }
