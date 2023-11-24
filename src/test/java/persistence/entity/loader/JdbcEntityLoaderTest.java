ee3cfe2e5f8ca906ed54f7009ae0f92d69686790
Jivebread <spark@troy.edu>
2023-11-24 22:43:44 +0900

    LoaderTest(Loader): LoaderTest에 있는 생성자들에 MetaEntity를 넣어줍니다.

--- src/test/java/persistence/entity/loader/JdbcEntityLoaderTest.java
+++ src/test/java/persistence/entity/loader/JdbcEntityLoaderTest.java
@@ -33,7 +33,7 @@ public class JdbcEntityLoaderTest extends BuilderTest {
   @Test
   @DisplayName("Loader를 이용해서 find 합니다.")
   public void findEntity() {
-    JdbcEntityLoader<PersonFixtureStep3> jdbcEntityLoader = new JdbcEntityLoader<>(PersonFixtureStep3.class, jdbcTemplate);
+    JdbcEntityLoader<PersonFixtureStep3> jdbcEntityLoader = new JdbcEntityLoader<>(meta, jdbcTemplate);
     PersonFixtureStep3 person = jdbcEntityLoader.load(ID1).get();
 
     assertThat(person).isEqualTo(두번째사람);
@@ -42,7 +42,7 @@ public class JdbcEntityLoaderTest extends BuilderTest {
   @Test
   @DisplayName("Loader를 이용해서 findAll 합니다.")
   public void findAllEntity() {
-    JdbcEntityLoader<PersonFixtureStep3> jdbcEntityLoader = new JdbcEntityLoader<>(PersonFixtureStep3.class, jdbcTemplate);
+    JdbcEntityLoader<PersonFixtureStep3> jdbcEntityLoader = new JdbcEntityLoader<>(meta, jdbcTemplate);
 
     List<PersonFixtureStep3> people = jdbcEntityLoader.findAll();
 
