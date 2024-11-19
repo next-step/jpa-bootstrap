1d52068e7b5b6b162fc2030b2b6352d0836c09ed
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-11-19 22:35:47 +0900

    fix: listener를 List로 받게 변경하고 List안에 담겨있는 Listener 전체를 실행하도록 수정

--- src/main/java/event/EventListenerGroup.java
+++ src/main/java/event/EventListenerGroup.java
@@ -1,44 +1,31 @@
 package event;
 
-import boot.Metamodel;
 import builder.dml.EntityData;
-import event.action.ActionQueue;
-import event.listener.*;
-import persistence.EntityLoader;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 
 public class EventListenerGroup<T> {
 
-    private final Map<EventType, EventListener<?>> eventListeners = new HashMap<>();
-    private ActionQueue actionQueue;
+    private final List<T> listeners = new ArrayList<>();
 
-    public EventListenerGroup(Metamodel metamodel, EntityLoader entityLoader) {
-        this.eventListeners.put(EventType.LOAD, new LoadEventListenerImpl<>(entityLoader));
-        this.eventListeners.put(EventType.PERSIST, new PersistEventListenerImpl<>(metamodel));
-        this.eventListeners.put(EventType.MERGE, new MergeEventListenerImpl<>(metamodel));
-        this.eventListeners.put(EventType.DELETE, new DeleteEventListenerImpl<>(metamodel));
-    }
-
-    @SuppressWarnings("unchecked")
-    public T handleEvent(EventType eventType, EntityData entityData) {
-        return (T) this.eventListeners.get(eventType).handleEvent(entityData);
-    }
-
-    public void execute() {
-        this.actionQueue.execute();
-    }
-
-    public void setActionQueue(ActionQueue actionQueue) {
-        this.actionQueue = actionQueue;
-        setActionQueueAllListener();
-    }
-
-    private void setActionQueueAllListener() {
-        for (EventListener<?> listener : eventListeners.values()) {
-            listener.setActionQueue(this.actionQueue);
+    public void handleEvent(EntityData entityData, BiConsumer<T, EntityData> biConsumer) {
+        for (T listener : listeners) {
+            biConsumer.accept(listener, entityData);
         }
+    }
+
+    public <R> R handleEventWithReturn(EntityData entityData, BiFunction<T, EntityData, R> action) {
+        for (T listener : listeners) {
+            return action.apply(listener, entityData);
+        }
+        return null;
+    }
+
+    public void addListener(T listener) {
+        this.listeners.add(listener);
     }
 
 }
